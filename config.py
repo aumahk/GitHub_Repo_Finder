fad4553a28c48b7cff029d6e826d2aefec865ca8
aumahk <141506984+aumahk@users.noreply.github.com>
2025-04-21 21:42:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+def load_token_from_env():
+    load_dotenv()
+    return os.getenv("GITHUB_TOKEN", "")
 
-def get_github_token():
-    # Local token if available (for developer only)
-    token = os.getenv("GITHUB_TOKEN")
-    if token:
-        return token
-
-    # Prompt user for token
-    import tkinter.simpledialog as simpledialog
-    return simpledialog.askstring("GitHub Token", "Enter your GitHub Personal Access Token:")
